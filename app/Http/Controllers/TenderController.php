4e4befaea5157cba92011555071a86bacc5eaf95
runism <selengetu@gmail.com>
2022-06-23 21:40:23 +0800

    tender

--- app/Http/Controllers/TenderController.php
+++ app/Http/Controllers/TenderController.php
@@ -14,12 +14,11 @@ class TenderController extends Controller {
     public function saveTender(Request $request)
     {
         DB::insert("insert into Tenders
-        (TENDERNO, TENDERTYPECODE, TENDERSELECTIONCODE, TENDER_CALL_AT, TENDER_OPEN_AT, TENDER_BUDGET, TENDER_BUDGET_SOURCE, TENDERTITLE, TENDER_INVITATIONCODE, TENDER_INVITATION_AT, TENDER_VALIDDATE, PACKCOUNT,
-         ASSESSMENT, TENDER_PLAYER,TENDER_STATE, ASSESSMENT_AT,STATEMENT_AT,CONTRACT_AT,SUSPENDED_AT,COMPLAINT_AT,ORDER_ID)
+        (TENDERNO, TENDERTYPECODE, TENDERSELECTIONCODE, TENDER_CALL_AT, TENDER_OPEN_AT, TENDER_BUDGET, TENDERTITLE, TENDER_INVITATIONCODE, TENDER_INVITATION_AT, TENDER_VALIDDATE, PACKCOUNT,
+         ASSESSMENT, TENDER_STATE, ASSESSMENT_AT , STATEMENT_AT, ORDER_ID)
         values
-        ($request->tenderno, $request->tendertypecode,$request->tenderselectioncode, '$request->tender_call_at', '$request->tender_open_at', '$request->tender_budget', '1', '$request->tendertitle', '$request->tender_invitationcode',
-        '$request->tender_invitation_at','$request->tender_validdate', $request->packcount,'$request->assessment', '$request->tender_player',$request->tender_state, '$request->assesstment_at','$request->statement_at',
-       '$request->contract_at','$request->suspended_at','$request->complaint_at','$request->order_id')");
+        ('$request->tenderno', '$request->tendertypecode','$request->tenderselectioncode', '$request->tender_call_at', '$request->tender_open_at', '$request->tender_budget', '$request->tendertitle', '$request->tender_invitationcode',
+        '$request->tender_invitation_at','$request->tender_validdate', '$request->packcount','$request->assessment','$request->tender_state', '$request->assesstment_at','$request->order_id')");
         return 1;
     }
     
@@ -30,5 +29,15 @@ class TenderController extends Controller {
     public function getTender($hid)
     {
         return DB::select("select * from V_TENDERS t where t.tenderid='$hid'");
+    }
+    public function saveTenderpack(Request $request)
+    {
+        DB::insert("insert into Tenders
+        (TENDERNO, TENDERTYPECODE, TENDERSELECTIONCODE, TENDER_CALL_AT, TENDER_OPEN_AT, TENDER_BUDGET, TENDERTITLE, TENDER_INVITATIONCODE, TENDER_INVITATION_AT, TENDER_VALIDDATE, PACKCOUNT,
+         ASSESSMENT, TENDER_STATE, ASSESSMENT_AT , STATEMENT_AT, ORDER_ID)
+        values
+        ('$request->tenderno', '$request->tendertypecode','$request->tenderselectioncode', '$request->tender_call_at', '$request->tender_open_at', '$request->tender_budget', '$request->tendertitle', '$request->tender_invitationcode',
+        '$request->tender_invitation_at','$request->tender_validdate', '$request->packcount','$request->assessment','$request->tender_state', '$request->assesstment_at','$request->order_id')");
+        return 1;
     }
 }
--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -461,7 +461,7 @@ button, input, optgroup, select, textarea {
                     <div class="card-header">
                         <h3 class="card-title">Явцын мэдээлэл</h3>
                         <div class="card-tools">
-                        <button class='btn btn-primary btn-xs' data-toggle="modal" data-target="#packModal"><i class='fa fa-plus'></i></button>
+                        <button class='btn btn-primary btn-xs' data-toggle="modal" data-target="#progressModal"><i class='fa fa-plus'></i></button>
                         </div>
                     </div>
                     <div class="card-body">
@@ -469,13 +469,10 @@ button, input, optgroup, select, textarea {
                     <br>
                     <table class="table table-bordered table-hover table-striped" id="infopack">
                         <thead >                        
-                            <th>Багцын нэр</th>
                             <th>Огноо</th>
-                            <th>Төсөвт өртөг</th>
-                            <th>Төлөв</th>
-                            <th>Гэрээ байгуулах эрх огноо</th>
-                            <th>Түдгэлзүүлсэн огноо</th>
-                            <th>Гомдол гаргасан огноо</th>
+                            <th>Явцын төлөв</th>
+                            <th>Тайлбар</th>
+                            <th>Ажилтан</th>
                             <th></th>
                         </thead>
                         <tbody id="tbody">                   
@@ -488,7 +485,7 @@ button, input, optgroup, select, textarea {
                     <div class="card-header">
                         <h3 class="card-title">Үнэлгээ/мэдэгдэл</h3>
                         <div class="card-tools">
-                        <button class='btn btn-primary btn-xs' data-toggle="modal" data-target="#packModal"><i class='fa fa-plus'></i></button>
+                        <button class='btn btn-primary btn-xs' data-toggle="modal" data-target="#complaintModal"><i class='fa fa-plus'></i></button>
                         </div>
                     </div>
                     <div class="card-body">
@@ -496,13 +493,9 @@ button, input, optgroup, select, textarea {
                     <br>
                     <table class="table table-bordered table-hover table-striped" id="infopack">
                         <thead >                        
-                            <th>Багцын нэр</th>
                             <th>Огноо</th>
-                            <th>Төсөвт өртөг</th>
                             <th>Төлөв</th>
-                            <th>Гэрээ байгуулах эрх огноо</th>
-                            <th>Түдгэлзүүлсэн огноо</th>
-                            <th>Гомдол гаргасан огноо</th>
+                            <th>Тайлбар</th>
                             <th></th>
                         </thead>
                         <tbody id="tbody">                   
@@ -853,12 +846,7 @@ button, input, optgroup, select, textarea {
                                 <input type="text" class="form-control" id="assessment" name="assessment">
                             </div>
                         </div> 
-                        <div class="col-8">
-                            <div class="form-group">
-                                <label for="jobname">Тендерт оролцогч</label>
-                                <input type="text" class="form-control" id="tender_player" name="tender_player" placeholder="Тендерт оролцогч">
-                            </div>
-                        </div>
+                       
                         <div class="col-4">
                             <div class="form-group">
                                 <label for="jobname">Тендерийн явц</label>
@@ -874,6 +862,161 @@ button, input, optgroup, select, textarea {
                             <div class="form-group">
                                 <label for="jobname">Тайлбар</label>
                                 <input type="text" class="form-control" id="tender_comment" name="tender_comment" placeholder="Тайлбар">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden"  id="tender_id" name="tender_id">
+                    @csrf
+                    <button type="submit" class="btn btn-primary">Хадгалах</button>
+                </div>
+                </form>
+            </div>
+        </div>
+        </div>
+        <div class="modal fade" id="packModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Багц бүртгэл</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form method="POST" id="formTender" action={{ route('saveTender') }}>
+                <div class="modal-body">
+               
+                    <div class="row">
+                        
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="jobname">Багцын нэр</label>
+                                <input type="text" class="form-control" id="tenderno" name="tenderno" placeholder="Тендерийн №">
+                                <input type="hidden" class="form-control" id="torder_id" name="torder_id" placeholder="Тендерийн №">
+                            </div>
+                        </div>
+                    
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="jobname">Огноо</label>
+                                <input type="date" class="form-control" id="tender_call_at" name="tender_call_at" >
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="jobname">Төсөвт өртөг</label>
+                                <input type="number" class="form-control" id="tender_budget" name="tender_budget" >
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="form-group">
+                                <label for="jobname">Төлөв</label>
+                                <select class="form-control" name="tender_state" id="tender_state" >
+                               
+                            </select>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="form-group">
+                                <label for="jobname">Гэрээ байгуулах эрх огноо</label>
+                                <input type="date" class="form-control" id="tender_invitationcode" name="tender_invitationcode">
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="form-group">
+                                <label for="jobname">Түдгэлзүүлсэн огноо</label>
+                                <input type="date" class="form-control" id="tender_invitation_at" name="tender_invitation_at" >
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="form-group">
+                                <label for="jobname">Гомдол гаргасан огноо</label>
+                                <input type="date" class="form-control" id="tender_open_at" name="tender_open_at">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden"  id="tender_id" name="tender_id">
+                    @csrf
+                    <button type="submit" class="btn btn-primary">Хадгалах</button>
+                </div>
+                </form>
+            </div>
+        </div>
+        </div>
+        <div class="modal fade" id="progressModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Явцын мэдээлэл</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form method="POST" id="formTender" action={{ route('saveTender') }}>
+                <div class="modal-body">
+               
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="jobname">Огноо</label>
+                                <input type="date" class="form-control" id="tender_call_at" name="tender_call_at" >
+                            </div>
+                        </div>
+                       
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="jobname">Төлөв</label>
+                                <select class="form-control" name="tender_state" id="tender_state" >
+                               
+                            </select>
+                            </div>
+                        </div>
+                      
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="jobname">Тайлбар</label>
+                                <input type="text" class="form-control" id="tender_open_at" name="tender_open_at">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden"  id="tender_id" name="tender_id">
+                    @csrf
+                    <button type="submit" class="btn btn-primary">Хадгалах</button>
+                </div>
+                </form>
+            </div>
+        </div>
+        </div>
+        <div class="modal fade" id="complaintModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Мэдэгдэл/Үнэлгээ бүртгэл</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form method="POST" id="formTender" action={{ route('saveTender') }}>
+                <div class="modal-body">
+               
+                    <div class="row">
+                        
+                    <div class="col-4">
+                            <div class="form-group">
+                                <label for="jobname">Огноо</label>
+                                <input type="date" class="form-control" id="tender_call_at" name="tender_call_at" >
+                            </div>
+                        </div>
+                      
+                        <div class="col-8">
+                            <div class="form-group">
+                                <label for="jobname">Тайлбар</label>
+                                <input type="text" class="form-control" id="tender_open_at" name="tender_open_at">
                             </div>
                         </div>
                     </div>
