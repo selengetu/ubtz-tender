d68826297b9cd0a7a3917d4c85b70a063ef7f1a9
selenge <selengetu@gmail.com>
2022-06-24 16:59:52 +0800

    pack

--- app/Http/Controllers/TenderController.php
+++ app/Http/Controllers/TenderController.php
@@ -43,14 +43,5 @@ class TenderController extends Controller {
     {
         return DB::select("select * from V_TENDERS t where t.tenderid='$hid'");
     }
-    public function saveTenderpack(Request $request)
-    {
-        DB::insert("insert into Tenders
-        (TENDERNO, TENDERTYPECODE, TENDERSELECTIONCODE, TENDER_CALL_AT, TENDER_OPEN_AT, TENDER_BUDGET, TENDERTITLE, TENDER_INVITATIONCODE, TENDER_INVITATION_AT, TENDER_VALIDDATE, PACKCOUNT,
-         ASSESSMENT, TENDER_STATE, ASSESSMENT_AT ,ORDER_ID)
-        values
-        ('$request->tenderno', '$request->tendertypecode','$request->tenderselectioncode', '$request->tender_call_at', '$request->tender_open_at', '$request->tender_budget', '$request->tendertitle', '$request->tender_invitationcode',
-        '$request->tender_invitation_at','$request->tender_validdate', '$request->packcount','$request->assessment','$request->tender_state', '$request->assesstment_at','$request->dorder_id')");
-        return 1;
-    }
+
 }
